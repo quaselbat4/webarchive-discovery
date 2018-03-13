211bd5e569773b1c343d4c83f11a890ab3e41fd2
Thomas Egense <thomas.egense@gmail.com>
2018-03-13 08:32:01 +0100

    Twitter parser+Analyse implemented. Still needs to extract more fields.

--- warc-indexer/src/main/java/uk/bl/wa/analyser/payload/TwitterAnalyser.java
+++ warc-indexer/src/main/java/uk/bl/wa/analyser/payload/TwitterAnalyser.java
@@ -26,35 +26,105 @@ package uk.bl.wa.analyser.payload;
  */
 
 import com.typesafe.config.Config;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.archive.io.ArchiveRecordHeader;
+
+import uk.bl.wa.parsers.TwitterParser;
+import uk.bl.wa.solr.SolrFields;
 import uk.bl.wa.solr.SolrRecord;
 import uk.bl.wa.util.Instrument;
+import uk.bl.wa.util.Normalisation;
 
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Analyzer to Twitter tweets harvested by API and packed as WARC by https://github.com/netarchivesuite/so-me.
  */
 public class TwitterAnalyser extends AbstractPayloadAnalyser {
-	private static Log log = LogFactory.getLog( TwitterAnalyser.class );
+  private static Log log = LogFactory.getLog( TwitterAnalyser.class );
+  private boolean normaliseLinks;
+  private boolean extractImageLinks;
 
-	public TwitterAnalyser(Config conf ) {
-//	    this.extractLinks = conf.getBoolean( "warc.index.extract.linked.resources" );
-		// TODO: Add setup
-	}
+  public TwitterAnalyser(Config conf ) {      
+    this.extractImageLinks = conf.getBoolean( "warc.index.extract.linked.images" );
+    this.normaliseLinks = conf.hasPath(uk.bl.wa.parsers.HtmlFeatureParser.CONF_LINKS_NORMALISE) ?
+        conf.getBoolean(uk.bl.wa.parsers.HtmlFeatureParser.CONF_LINKS_NORMALISE) :
+          uk.bl.wa.parsers.HtmlFeatureParser.DEFAULT_LINKS_NORMALISE;
+  }
 
-	@Override
-    public void analyse(ArchiveRecordHeader header, InputStream content, SolrRecord solr) {
-        final long start = System.nanoTime();
-        log.debug("Performing Twitter tweet analyzing");
-		// TODO: Map Twitter-fields to Solr equivalents
-		// content is guaranteed to be a Twitter tweet in JSON
-		// https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/intro-to-tweet-json
+  @Override
+  public void analyse(ArchiveRecordHeader header, InputStream content, SolrRecord solr) {
+    final long start = System.nanoTime();
+    log.debug("Performing Twitter tweet analyzing");
+    // TODO: Map Twitter-fields to Solr equivalents
+    // content is guaranteed to be a Twitter tweet in JSON
+    // https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/intro-to-tweet-json
 
-//		solr.addField( SolrFields.SOLR_LINKS_IMAGES, urlNorm);
-        Instrument.timeRel("WARCPayloadAnalyzers.analyze#total", "TwitterAnalyzer.analyze#total", start);
+    String json = null;
+    try{
+      json = IOUtils.toString(content, "UTF-8");
+      TwitterParser parser = new TwitterParser(json);  //All fields mapped to java
+      solr.addField(SolrFields.SOLR_AUTHOR, parser.getAuthor());
+      Date modified = parser.getCreateDate();                
+
+      int year = Integer.parseInt(new SimpleDateFormat("yyyy").format(modified)); //date.getYear() deprecated
+      solr.addField( SolrFields.LAST_MODIFIED_YEAR, ""+year);                 
+      solr.addField(SolrFields.LAST_MODIFIED, getSolrTimeStamp(modified));
+      //solr.addField(SolrFields.SOLR_EXTRACTED_TEXT,parser.getContent()); //already set from warc-indexer 
+
+      if (extractImageLinks){
+
+        ArrayList<String> imageUrlFormatted = formatUrls(parser.getImageUrlsList());
+        for (String urlFormated : imageUrlFormatted){           
+          solr.addField( SolrFields.SOLR_LINKS_IMAGES, urlFormated);
+        }
+      }
+
+      for (String hashTag : parser.getHashTagsList()){                 
+        solr.addField( SolrFields.SOLR_KEYWORDS,hashTag);
+      }
+
+
     }
-	
+    catch(Exception e){
+      log.error("Error parsing twitter Json: " + e.getMessage());
+      solr.addParseException("Error parsing twitter Json", e);
+      return;
+    }
+
+
+    Instrument.timeRel("WARCPayloadAnalyzers.analyze#total", "TwitterAnalyzer.analyze#total", start);
+  }
+
+
+  private  ArrayList<String> formatUrls(ArrayList<String > urls){	    
+    if (!normaliseLinks){ //Do nothing. Not recommended, but has to follow the config.
+      return urls;
+    }
+    else{
+      ArrayList<String> urlNorms = new ArrayList<String>();
+      for (String url :urls){
+        String urlNorm =  Normalisation.canonicaliseURL(url);	            
+        urlNorms.add(urlNorm);
+      }	      	      
+      return urlNorms;
+    }
+  }
+
+  private String getSolrTimeStamp(Date date){
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"); //not thread safe, so create new         
+    dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));    
+    return dateFormat.format(date)+"Z";
+
+  }
+
+
 }
--- warc-indexer/src/main/java/uk/bl/wa/parsers/TwitterParser.java
+++ warc-indexer/src/main/java/uk/bl/wa/parsers/TwitterParser.java
@@ -1,5 +1,27 @@
 package uk.bl.wa.parsers;
 
+/*
+ * #%L
+ * warc-indexer
+ * %%
+ * Copyright (C) 2013 - 2018 The UK Web Archive
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -16,50 +38,86 @@ public class TwitterParser {
   private ArrayList<String> imageUrlsList;
   private ArrayList<String> hashTagsList;
   private String content;
-  
+
   public TwitterParser(String json) throws Exception{
     
     imageUrlsList = new  ArrayList<String>();
     hashTagsList = new  ArrayList<String>();
-         
+
     JSONObject full = new JSONObject(json);        
-    String text =  full.getString("full_text");
+    String text =  "";
+
+    if (full.has("full_text")){
+      //System.out.println("fulltext case");
+      text = full.getString("full_text");
+    }    
+    else{
+      //System.out.println("text case");
+      text =full.getString("text"); //legacy 
+    }
+
     this.content=text;
-    
-    JSONObject entities = full.getJSONObject("entities");
+
+
+    JSONObject entities; // Getting the entities require many special cases. Sometimes they are double, need to read into specification
+
+    if (full.has("retweeted_status")) {
+      //System.out.println("retweeted case");
+      JSONObject retweet = full.getJSONObject("retweeted_status");
+      if (retweet.has("extended_tweet")){
+        entities = retweet.getJSONObject("extended_tweet").getJSONObject("entities");
+      }
+      else{
+        entities = retweet.getJSONObject("entities");
+
+      }           
+    }
+    else if (full.has("entities")){     
+      entities = full.getJSONObject("entities");            
+      //System.out.println("entities case");
+    }
+    else{
+      throw new Exception("could not find entities on twitter JSON");
+    }
+
+
+    //media(images), not always there.
+    if (entities.has("media")){
+      JSONArray media = entities.getJSONArray("media");   
+      for (int i = 0;i<media.length();i++){  //images
+        JSONObject medie= media.getJSONObject(i);
+
+        String type =  medie.getString("type");
+        if ("photo".equals(type)){
+          String imageUrl =  medie.getString("media_url");
+          imageUrlsList.add(imageUrl);        
+          System.out.println("found image:"+imageUrl);
+        }
+      }               
+    }
+
     JSONArray hashTags = entities.getJSONArray("hashtags");
-    
+
     for (int i = 0;i<hashTags.length();i++){ //keywords
       String tag =  ((JSONObject) hashTags.get(i)).getString("text");
-      hashTagsList.add(tag);       
+      hashTagsList.add(tag);      
     }
-    
-    JSONArray media = entities.getJSONArray("media");
-    
-    for (int i = 0;i<media.length();i++){  //images
-      JSONObject medie= media.getJSONObject(i);
-      
-      String type =  medie.getString("type");
-      if ("photo".equals(type)){
-        String imageUrl =  medie.getString("media_url");
-        imageUrlsList.add(imageUrl);        
-      }
-      
-      JSONObject user= full.getJSONObject("user"); 
-      String author = user.getString("name");
-      this.author=author;
- 
-      
-      //Format Fri Mar 02 10:26:13 +0000 2018
-      String created_at_str = full.getString("created_at");
-            
-      DateFormat df = new SimpleDateFormat("EEE MMM dd kk:mm:ss Z yyyy", Locale.ENGLISH);
-      Date created_at =  df.parse(created_at_str);        
-      this.createDate=created_at;
-     
-      //TODO
-      //set language/content_type            
-    }   
+
+    JSONObject user= full.getJSONObject("user"); 
+    String author = user.getString("name");
+    this.author=author;
+
+
+    //Format Fri Mar 02 10:26:13 +0000 2018
+    String created_at_str = full.getString("created_at");
+
+    DateFormat df = new SimpleDateFormat("EEE MMM dd kk:mm:ss Z yyyy", Locale.ENGLISH);
+    Date created_at =  df.parse(created_at_str);        
+    this.createDate=created_at;
+
+    //TODO
+    //set language/content_type ?            
+
   }
 
   public String getAuthor() {
@@ -81,6 +139,6 @@ public class TwitterParser {
   public String getContent() {
     return content;
   }
-  
-  
+
+
 }
