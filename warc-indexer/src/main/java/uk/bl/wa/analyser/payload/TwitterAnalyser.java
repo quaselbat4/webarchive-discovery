0f28542df53b114acb8d19b2a41c842a3820830c
Toke Eskildsen <te@ekot.dk>
2020-05-18 13:19:45 +0200

    Expand the paths to check for embedded images

--- warc-indexer/src/main/java/uk/bl/wa/analyser/payload/TwitterAnalyser.java
+++ warc-indexer/src/main/java/uk/bl/wa/analyser/payload/TwitterAnalyser.java
@@ -52,21 +52,21 @@ public class TwitterAnalyser extends AbstractPayloadAnalyser implements JSONExtr
 
     // TODO: These dynamic fields does not have docValues in the current schema. Hopefully fixed when switching to the Solr 7 schema, but might require adjustments
     // TODO: Does the user-information belong in the same document as the Tweet? Shouldn't it be in a separate document (and linked somehow)?
-    public static final String MENTIONS = "user_mentions_ss";
-    public static final String SCREEN_NAME = "user_screen_name_ss";
-    public static final String USER_URL = "user_url_ss";
-    public static final String USER_ID = "user_id_tls";
-    public static final String FOLLOWERS_COUNT = "user_followers_count_is";
-    public static final String FRIENDS_COUNT = "user_friends_count_is";
-    public static final String FAVOURITES_COUNT = "user_favourites_count_is";
-    public static final String STATUSES_COUNT = "user_statuses_count_is";
-    public static final String VERIFIED = "user_verified_bs";
-    public static final String DESCRIPTION = "user_description_t";
+    public static final String MENTIONS =          "user_mentions_ss";
+    public static final String SCREEN_NAME =       "user_screen_name_ss";
+    public static final String USER_URL =          "user_url_ss";
+    public static final String USER_ID =           "user_id_tls";
+    public static final String FOLLOWERS_COUNT =   "user_followers_count_is";
+    public static final String FRIENDS_COUNT =     "user_friends_count_is";
+    public static final String FAVOURITES_COUNT =  "user_favourites_count_is";
+    public static final String STATUSES_COUNT =    "user_statuses_count_is";
+    public static final String VERIFIED =          "user_verified_bs";
+    public static final String DESCRIPTION =       "user_description_t";
 
-    public static final String RETWEETED_COUNT = "retweeted_count_is";
-    public static final String TWEET_ID = "tweet_id_tls";
+    public static final String RETWEETED_COUNT =   "retweeted_count_is";
+    public static final String TWEET_ID =          "tweet_id_tls";
     public static final String REPLY_TO_TWEET_ID = "reply_to_tweet_id_tls";
-    public static final String REPLY_TO_USER_ID = "reply_to_user_id_tls";
+    public static final String REPLY_TO_USER_ID =  "reply_to_user_id_tls";
 
     private boolean normaliseLinks;
 
@@ -78,43 +78,38 @@ public class TwitterAnalyser extends AbstractPayloadAnalyser implements JSONExtr
 
     private final JSONExtractor extractor = new JSONExtractor();
     { // We always do these
-        extractor.add(SolrFields.SOLR_AUTHOR, true, this, ".user.name");
-        extractor.add(SCREEN_NAME, true, this, ".user.screen_name");
-        extractor.add(USER_URL, true, this, ".user.url");
-        extractor.add(FOLLOWERS_COUNT, true, this, ".user.followers_count");
-        extractor.add(FRIENDS_COUNT, true, this, ".user.friends_count");
-        extractor.add(FAVOURITES_COUNT, true, this, ".user.favourites_count");
-        extractor.add(STATUSES_COUNT, true, this, ".user.statuses_count");
-        extractor.add(VERIFIED, true, this, ".user.verified");
-        extractor.add(DESCRIPTION, true, this, ".user.description");
-        extractor.add(USER_ID, true, this, ".user.id_str");
+        extractor.add(SolrFields.SOLR_AUTHOR,   true, this, ".user.name");
+        extractor.add(SCREEN_NAME,              true, this, ".user.screen_name");
+        extractor.add(USER_URL,                 true, this, ".user.url");
+        extractor.add(FOLLOWERS_COUNT,          true, this, ".user.followers_count");
+        extractor.add(FRIENDS_COUNT,            true, this, ".user.friends_count");
+        extractor.add(FAVOURITES_COUNT,         true, this, ".user.favourites_count");
+        extractor.add(STATUSES_COUNT,           true, this, ".user.statuses_count");
+        extractor.add(VERIFIED,                 true, this, ".user.verified");
+        extractor.add(DESCRIPTION,              true, this, ".user.description");
+        extractor.add(USER_ID,                  true, this, ".user.id_str");
 
-        extractor.add(RETWEETED_COUNT, true, this, ".retweeted_count");
-        extractor.add(TWEET_ID, true, this, ".id_str");
-        extractor.add(REPLY_TO_TWEET_ID, true, this, ".in_reply_to_status_id_str");
-        extractor.add(REPLY_TO_USER_ID, true, this, ".in_reply_to_user_id_str");
+        extractor.add(RETWEETED_COUNT,          true, this, ".retweeted_count");
+        extractor.add(TWEET_ID,                 true, this, ".id_str");
+        extractor.add(REPLY_TO_TWEET_ID,        true, this, ".in_reply_to_status_id_str");
+        extractor.add(REPLY_TO_USER_ID,         true, this, ".in_reply_to_user_id_str");
         extractor.add(SolrFields.LAST_MODIFIED, true, this, ".created_at");
-        extractor.add(SolrFields.SOLR_EXTRACTED_TEXT, true, this,
-                      ".retweeted_status.extended_tweet.full_text",
-                      ".retweeted_status.text",
-                      ".extended_tweet.full_text",
-                      ".text");
-        extractor.add(SolrFields.SOLR_LINKS, false, this,
-                      ".extended_tweet.entities.urls[].expanded_url",
-                      ".extended_tweet.extended_entities.media[].video_info.variants[].url",
-                      ".extended_tweet.entities.media[].video_info.variants[].url",
-                      ".retweeted_status.entities.urls[].expanded_url",
-                      ".entities.urls[].expanded_url");
-        extractor.add(SolrFields.SOLR_KEYWORDS, false, this,
-                      ".retweeted_status.extended_tweet.entities.hashtags[].text",
-                      ".extended_tweet.entities.hashtags[].text",
-                      ".retweeted_status.entities.hashtags[].text",
-                      ".entities.hashtags[].text");
-        extractor.add(MENTIONS, false, this,
-                      ".retweeted_status.extended_tweet.entities.user_mentions[].screen_name",
-                      ".extended_tweet.entities.user_mentions[].screen_name",
-                      ".retweeted_status.entities.user_mentions[].screen_name",
-                      ".entities.user_mentions[].screen_name");
+
+        extractor.add(SolrFields.SOLR_EXTRACTED_TEXT, true, this, expandPaths(
+                ".extended_tweet.full_text",
+                ".text"
+        ));
+        extractor.add(SolrFields.SOLR_LINKS, false, this, expandPaths(
+                ".entities.urls[].expanded_url",
+                ".extended_entities.media[].video_info.variants[].url",
+                ".entities.media[].video_info.variants[].url"
+        ));
+        extractor.add(SolrFields.SOLR_KEYWORDS, false, this, expandPaths(
+                ".entities.hashtags[].text"
+        ));
+        extractor.add(MENTIONS, false, this, expandPaths(
+                ".entities.user_mentions[].screen_name"
+        ));
     }
 
     // Needed by the Analyser-resolver
@@ -131,14 +126,43 @@ public class TwitterAnalyser extends AbstractPayloadAnalyser implements JSONExtr
                 conf.getBoolean(uk.bl.wa.parsers.HtmlFeatureParser.CONF_LINKS_NORMALISE) :
                 uk.bl.wa.parsers.HtmlFeatureParser.DEFAULT_LINKS_NORMALISE;
         if (conf.getBoolean("warc.index.extract.linked.images")) {
-            extractor.add(SolrFields.SOLR_LINKS_IMAGES, false, this,
-                          ".extended_tweet.entities.media[].media_url_https",
-                          ".extended_tweet.entities.media[].media_url",
-                          ".extended_entities.media[].media_url_https",
-                          ".extended_entities.media[].media_url",
-                          ".entities.media[].media_url_https",
-                          ".entities.media[].media_url");
+            extractor.add(SolrFields.SOLR_LINKS_IMAGES, false, this, expandPaths(
+                    ".user.profile_image_url_https",
+                    ".user.profile_banner_url",
+                    ".user.profile_background_image_url_https",
+
+                    ".extended_tweet.entities.media[].media_url_https",
+                    ".extended_tweet.entities.media[].media_url",
+                    ".extended_entities.media[].media_url_https",
+                    ".extended_entities.media[].media_url",
+                    ".entities.media[].media_url_https",
+                    ".entities.media[].media_url"
+            ));
         }
+    }
+
+    /**
+     * Tweet content can be plain, quoted, retweeted or both retweeted & quoted.
+     *
+     * This helper takes the given paths and permutates them with the prefixes
+     * {@code } (empty prefix)
+     * {@code .quoted_tweet}
+     * {@code .retweeted_status}
+     * {@code .retweeted_status.quoted_tweet}
+     * Also returns the original paths.
+     * @param paths a number of JSON paths.
+     * @return the paths permutated with the prefixes mentioned.
+     */
+    private String[] expandPaths(String... paths) {
+        final String[] PREFIXES = new String[]{
+                "", ".quoted_tweet", ".retweeted_status", ".retweeted_status.quoted_tweet"};
+        String[] permutations = new String[PREFIXES.length*paths.length];
+        for (int i = 0 ; i < paths.length ; i++) {
+            for (int j = 0 ; j < PREFIXES.length ; j++) {
+                permutations[(i*PREFIXES.length)+j] = PREFIXES[j] + paths[i];
+            }
+        }
+        return permutations;
     }
 
     @Override
